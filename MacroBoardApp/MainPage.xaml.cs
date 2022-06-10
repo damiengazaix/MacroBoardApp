9981ef53ab2d001c01cef07e273a15ef1842a754
Gazaix <damien.gazaix@etu.univ-amu.fr>
2022-06-10 15:47:39 +0200

    update

--- MacroBoardApp/MainPage.xaml.cs
+++ MacroBoardApp/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using System.IO;
 using System.Diagnostics;
 using System.Collections.ObjectModel;
 using Xamarin.Essentials;
+using System.Net.NetworkInformation;
 
 namespace MacroBoardApp
 {
@@ -28,6 +29,8 @@ namespace MacroBoardApp
 
         public Observable<bool> btnConnectIsEnable { get; set; } = new Observable<bool>(true);
         public Observable<bool> btnDisconnectIsEnable { get; set; } = new Observable<bool>(false);
+
+        private bool isConnected { get; set; } = false;
 
         public MainPage()
         {
@@ -55,10 +58,50 @@ namespace MacroBoardApp
 
             Thread thread = new Thread(() => Reload_Workflows());
             thread.Start();
+
+            Thread.Sleep(1000);
+
+            if (!isConnected)
+            {
+                
+                if (client != null)
+                {
+                    if (stream != null)
+                    {
+                        byte[] serverResponse = new byte[50];
+
+                        stream.Write(Encoding.ASCII.GetBytes("Quit"), 0, 4);
+                        stream.Read(serverResponse, 0, serverResponse.Length);
+                        Console.WriteLine(Encoding.ASCII.GetString(serverResponse));
+
+                        stream.Close();
+                    }
+
+                    if (client.GetStream() != null)
+                        client.GetStream().Close();
+
+                    client.Close();
+                }
+
+                client = null;
+                stream = null;
+
+                thread.Abort();
+
+                btnConnectIsEnable.Value = true;
+                btnDisconnectIsEnable.Value = false;
+                btnName.Value = "Connect";
+                
+            }
         }
 
         private void Btn_Disconnect_Clicked(object sender, EventArgs e)
         {
+            WfList.Clear();
+            btnName.Value = "Connect";
+
+            if (stream == null) return;
+
             byte[] serverResponse = new byte[50];
 
             stream.Write(Encoding.ASCII.GetBytes("Quit"), 0, 4);
@@ -72,14 +115,10 @@ namespace MacroBoardApp
 
             client = null;
             clientSender = null;
-
-            WfList.Clear();
-            btnName.Value = "Connect";
         }
 
         private void Reload_Workflows()
         {
-
             if(client == null)
             {
                 localAddr = IpBar.Text;
@@ -89,82 +128,98 @@ namespace MacroBoardApp
 
                 try
                 {
+                    isConnected = false;
                     client = new TcpClient(localAddr, 13000);
                     stream = client.GetStream();
+                    isConnected = true;
                 }
                 catch (SocketException se)
                 {
                     Console.WriteLine(se.Message);
                     btnConnectIsEnable.Value = true;
+                    btnDisconnectIsEnable.Value = false;
                     btnName.Value = "Connect";
                     return;
                 }
             }
 
-            btnDisconnectIsEnable.Value = true;
+            isConnected = true;
 
-            byte[] serverResponse = new byte[50];
-
-            stream.Write(Encoding.ASCII.GetBytes("Continue"), 0, 8);
-            stream.Read(serverResponse, 0, serverResponse.Length);
-            Console.WriteLine(Encoding.ASCII.GetString(serverResponse));
-
-            List<byte> imgData = new List<byte>();
-            byte[] imgLength = new byte[20];
-            byte[] nameLength = new byte[20];
-            byte[] nameData = new byte[50];
-
-            byte[] wfNumber = new byte[1];
-            stream.Read(wfNumber, 0, 1);
-
-            for (int i = Int16.Parse(Encoding.ASCII.GetString(wfNumber)); i > 0; i--)
+            try
             {
-                imgData = new List<byte>();
-                imgLength = new byte[20];
-                nameLength = new byte[20];
+                byte[] serverResponse = new byte[50];
 
-                stream.Read(imgLength, 0, imgLength.Length);
+                stream.Write(Encoding.ASCII.GetBytes("Continue"), 0, 8);
+                stream.Read(serverResponse, 0, serverResponse.Length);
+                Console.WriteLine(Encoding.ASCII.GetString(serverResponse));
 
-                byte[] confirmMsg = Encoding.ASCII.GetBytes("Image Length Receive");
+                List<byte> imgData = new List<byte>();
+                byte[] imgLength = new byte[20];
+                byte[] nameLength = new byte[20];
+                byte[] nameData = new byte[50];
 
-                stream.Write(confirmMsg, 0, confirmMsg.Length);
+                byte[] wfNumber = new byte[1];
+                stream.Read(wfNumber, 0, 1);
 
-                int bytes;
-                int imgMaxLen = Int32.Parse(Encoding.ASCII.GetString(imgLength));
-
-                for (int count = 0; count < imgMaxLen; count++)
+                for (int i = Int16.Parse(Encoding.ASCII.GetString(wfNumber)); i > 0; i--)
                 {
-                    bytes = stream.ReadByte();
-                    imgData.Add((byte)bytes);
+                    imgData = new List<byte>();
+                    imgLength = new byte[20];
+                    nameLength = new byte[20];
+
+                    stream.Read(imgLength, 0, imgLength.Length);
+
+                    byte[] confirmMsg = Encoding.ASCII.GetBytes("Image Length Receive");
+
+                    stream.Write(confirmMsg, 0, confirmMsg.Length);
+
+                    int bytes;
+                    int imgMaxLen = Int32.Parse(Encoding.ASCII.GetString(imgLength));
+
+                    for (int count = 0; count < imgMaxLen; count++)
+                    {
+                        bytes = stream.ReadByte();
+                        imgData.Add((byte)bytes);
+                    }
+
+                    confirmMsg = Encoding.ASCII.GetBytes("Image Receive");
+                    stream.Write(confirmMsg, 0, confirmMsg.Length);
+
+                    stream.Read(nameLength, 0, nameLength.Length);
+
+                    int nameMaxLen = Int32.Parse(Encoding.ASCII.GetString(nameLength));
+                    nameData = new byte[nameMaxLen];
+                    stream.Read(nameData, 0, nameMaxLen);
+
+                    confirmMsg = Encoding.ASCII.GetBytes("Everything Received");
+                    stream.Write(confirmMsg, 0, confirmMsg.Length);
+
+                    WfList.Add(new Workflow(Encoding.ASCII.GetString(nameData, 0, nameData.Length),
+                        byteArrayToImage(imgData.ToArray()), widthPhone));
+
+                    Console.WriteLine("Everything Received");
+
+                    if (clientSender == null)
+                        clientSender = new TcpClient(localAddr, 14000);
+
+                    Trace.WriteLine("Connected");
+
+                    btnName.Value = "Refresh";
                 }
 
-                confirmMsg = Encoding.ASCII.GetBytes("Image Receive");
-                stream.Write(confirmMsg, 0, confirmMsg.Length);
-
-                stream.Read(nameLength, 0, nameLength.Length);
-
-                int nameMaxLen = Int32.Parse(Encoding.ASCII.GetString(nameLength));
-                nameData = new byte[nameMaxLen];
-                stream.Read(nameData, 0, nameMaxLen);
-
-                confirmMsg = Encoding.ASCII.GetBytes("Everything Received");
-                stream.Write(confirmMsg, 0, confirmMsg.Length);
-
-                WfList.Add(new Workflow(Encoding.ASCII.GetString(nameData, 0, nameData.Length),
-                    byteArrayToImage(imgData.ToArray()), widthPhone));
-
-                Console.WriteLine("Everything Received");
-
-                if (clientSender == null)
-                    clientSender = new TcpClient(localAddr, 14000);
-
-                Trace.WriteLine("Connected");
-
-                btnName.Value = "Refresh";
+                btnConnectIsEnable.Value = true;
+                btnDisconnectIsEnable.Value = false;
+            }
+            catch (Exception)
+            {
+                WfList.Clear();
+                btnConnectIsEnable.Value = true;
+                btnDisconnectIsEnable.Value = false;
+                btnName.Value = "Connect";
             }
 
-            btnConnectIsEnable.Value = true;
-            
+
+
         }
 
         public void BtnOnclick(object sender, EventArgs args)
@@ -219,5 +274,6 @@ namespace MacroBoardApp
             }
 
         }
+
     }
 }
